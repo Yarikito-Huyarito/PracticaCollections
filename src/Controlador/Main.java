d6b5b711a5fd20c8272db3829878cd240bfdc66a
Yarikito-Huyarito <xom9k2006@gmail.com>
2026-03-17 13:22:51 +0100

    2n commit

--- src/Controlador/Main.java
+++ src/Controlador/Main.java
@@ -7,6 +7,6 @@ import Vista.*;
 public class Main {
     public static Scanner scan =  new Scanner(System.in);
     public static void main(String[] args) {
-
+        System.out.println();
     }
 }
